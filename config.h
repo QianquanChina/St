27d248315d33ccbc1717c84305be01c2375ef2ce
Jc <370047797@qq.com>
2021-12-21 13:59:46 +0800

    nerd font for icon

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Monaco:pixelsize=16:antialias=true:autohint=true";
+static char *font = "CodeNewRoman Nerd Font Mono:pixelsize=19:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
