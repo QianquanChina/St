02a97161a51126d81cfc7feefb9af4c26354a6c0
jc <370047797@qq.com>
2022-04-09 16:38:27 +0800

    newterm patch

--- config.h
+++ config.h
@@ -203,9 +203,10 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,                 XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,               XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,                 XK_Num_Lock,    numlock,        {.i =  0} },
+	{ TERMMOD,                 XK_Return,      newterm,        {.i =  0} },
 
-	{ MODKEY_MY|ControlMask,   XK_k,           kscrollup,      {.i = -1} },
-	{ MODKEY_MY|ControlMask,   XK_j,           kscrolldown,    {.i = -1} },
+	{ MODKEY_MY|ShiftMask,     XK_K,           kscrollup,      {.i = -1} },
+	{ MODKEY_MY|ShiftMask,     XK_J,           kscrolldown,    {.i = -1} },
 
 	{ MODKEY_MY,               XK_k,           kscrollup,      {.i =  1} },
 	{ MODKEY_MY,               XK_j,           kscrolldown,    {.i =  1} },
